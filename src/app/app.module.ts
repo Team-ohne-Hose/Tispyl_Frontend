999daff77578864c29c05583709c96f6822cf28c
patagona-trettig <t.rettig@patagona.de>
2020-04-09 13:59:36 +0200

    Add initial implementation for register popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RulesComponent } from './rules/rules.component';
 import { GifViewerComponent } from './gif-viewer/gif-viewer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MessageComponent } from './message/message.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,11 +26,18 @@ import { GifViewerComponent } from './gif-viewer/gif-viewer.component';
     LoginComponent,
     RegisterComponent,
     RulesComponent,
-    GifViewerComponent
+    GifViewerComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gif-viewer/gif-viewer.component.ts
+++ src/app/gif-viewer/gif-viewer.component.ts
@@ -11,24 +11,14 @@ export class GifViewerComponent implements OnInit {
   constructor( private http: HttpClient) { }
 
   @Input() languageObjects: { };
-  const contentUrl = 'https://media0.giphy.com/media/5JEWBLv0mZDYA/giphy.gif?cid=ecf05e472c0346bcd92707d0d34358bf7af0b328676f018b&rid=giphy.gif';
-
-  const giphyURL = encodeURI('https://api.giphy.com/v1/gifs/random?api_key=0UTRbFtkMxAplrohufYco5IY74U8hOes&tag=rainbow');//&rating=r');
-
-
+  contentUrl = 'https://media0.giphy.com/media/5JEWBLv0mZDYA/giphy.gif?cid=ecf05e472c0346bcd92707d0d34358bf7af0b328676f018b&rid=giphy.gif';
+  giphyURL = encodeURI('https://api.giphy.com/v1/gifs/random?api_key=0UTRbFtkMxAplrohufYco5IY74U8hOes&tag=rainbow');//&rating=r');
 
   ngOnInit(): void {
     // this.getNewGif();
   }
 
   getNewGif(): void {
-    /*$.getJSON(this.giphyURL, json => {
-      console.log(json.data);
-      // Set gif as bg image
-      $('#gif-wrap').css({
-        'background-image': 'url("' + json.data.image_original_url + '")'
-      });
-    };*/
     this.http.get(this.giphyURL).subscribe((val) => {
       console.log(val.data.url);
       this.contentUrl = val.data.image_original_url;
