42cdd5317f7e4b512f690ac499d491a7e764b369
DanielLiebler <DLiebler13@gmail.com>
2020-04-09 21:59:36 +0200

    cleanup 3D

--- src/app/viewport/viewport.component.ts
+++ src/app/viewport/viewport.component.ts
@@ -10,25 +10,6 @@ import {AudioLoader, Camera, Renderer, Scene, TextureLoader} from 'three';
 export class ViewportComponent implements OnInit {
 
   constructor() { }
-  scene: Scene;
-  camera: Camera;
-  renderer: Renderer;
-
-  loader = new TextureLoader();
-  geometry = new THREE.BoxGeometry(40, .2, 40);
-  material = new THREE.MeshPhysicalMaterial( {color: 0xffffff});
-  cube = new THREE.Mesh(this.geometry, this.material);
-  listener = new THREE.AudioListener();
-  sound = new THREE.Audio(this.listener);
-  audioLoader = new AudioLoader();
-  hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-  hemiLightHelper = new THREE.HemisphereLightHelper( this.hemiLight, 10 );
-  dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-  dirLightHeper = new THREE.DirectionalLightHelper( this.dirLight, 10 );
-
-  groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
-  groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff} );
-  ground = new THREE.Mesh( this.groundGeo, this.groundMat );
 
   vertexShader = `varying vec3 vWorldPosition;
     void main() {
@@ -46,6 +27,25 @@ export class ViewportComponent implements OnInit {
       float h = normalize( vWorldPosition + offset ).y;
       gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
     }`;
+
+  gameBoardTextureURL: String = '/assets/tischspiel.png';
+
+  // Utilities
+  scene: Scene;
+  camera: Camera;
+  renderer: Renderer;
+  tLoader = new TextureLoader();
+
+  // Lighting
+  hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+  hemiLightHelper = new THREE.HemisphereLightHelper( this.hemiLight, 10 );
+  dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  dirLightHeper = new THREE.DirectionalLightHelper( this.dirLight, 10 );
+
+  // Environment
+  groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
+  groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff} );
+  ground = new THREE.Mesh( this.groundGeo, this.groundMat );
   uniforms = {
     'topColor': { value: new THREE.Color( 0x0077ff ) },
     'bottomColor': { value: new THREE.Color( 0xffffff ) },
@@ -58,26 +58,38 @@ export class ViewportComponent implements OnInit {
     vertexShader: this.vertexShader,
     fragmentShader: this.fragmentShader,
     side: THREE.BackSide
-  } );
+  });
   sky = new THREE.Mesh( this.skyGeo, this.skyMat );
+
+  // Sound
+  listener = new THREE.AudioListener();
+  sound = new THREE.Audio(this.listener);
+  audioLoader = new AudioLoader();
+
+  // Board
+  gameBoardGeo = new THREE.BoxGeometry(40, .2, 40);
+  gameBoardMat = new THREE.MeshPhysicalMaterial( {color: 0xffffff});
+  gameBoard = new THREE.Mesh(this.gameBoardGeo, this.gameBoardMat);
 
   animate() {
     requestAnimationFrame(this.animate.bind(this));
     this.renderer.render(this.scene, this.camera);
-    this.cube.rotation.y += 0.002;
+
+    this.gameBoard.rotation.y += 0.002;
   }
 
   ngOnInit(): void {
-    this.initScene();
+    this.initScene(window.innerWidth / window.innerHeight);
     this.loadTexture();
     this.initLighting();
-    this.cube.position.y = 0;
-    this.cube.castShadow = true;
-    this.cube.receiveShadow = true;
-    this.scene.add(this.cube);
 
-    this.camera.position.set( 0, 15, 20 );
-    this.camera.lookAt(0,1,0);
+    this.gameBoard.position.y = 0;
+    this.gameBoard.castShadow = true;
+    this.gameBoard.receiveShadow = true;
+    this.scene.add(this.gameBoard);
+
+    this.camera.position.set( 0, 10, 20 );
+    this.camera.lookAt(0,0,10);
 
 
     this.renderer.shadowMap.enabled = true;
@@ -86,9 +98,9 @@ export class ViewportComponent implements OnInit {
     this.animate();
   }
 
-  initScene(): void {
+  initScene(aspectRatio: number): void {
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 5000);
+    this.camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 5000);
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('viewport-container').appendChild( this.renderer.domElement );
@@ -97,10 +109,10 @@ export class ViewportComponent implements OnInit {
     this.scene.fog = new THREE.Fog( this.scene.background, 0.1, 5000 );
   }
   loadTexture(): void {
-    this.loader.load('/assets/tischspiel.png', (texture) => {
-      //texture.encoding = THREE.sRGBEncoding;
-      this.material.map = texture;
-      this.material.needsUpdate = true;
+    this.tLoader.load(this.gameBoardTextureURL, (texture) => {
+      // texture.encoding = THREE.sRGBEncoding;
+      this.gameBoardMat.map = texture;
+      this.gameBoardMat.needsUpdate = true;
     }, undefined, (error) => {
       console.error(error);
     });
@@ -109,36 +121,35 @@ export class ViewportComponent implements OnInit {
     this.hemiLight.color.setHSL( 0.6, 1, 0.6 );
     this.hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
     this.hemiLight.position.set( 0, 50, 0 );
-    this.scene.add( this.hemiLight );
 
+    this.scene.add( this.hemiLight );
     this.scene.add( this.hemiLightHelper );
 
     this.dirLight.color.setHSL( 0.1, 1, 0.95 );
     this.dirLight.position.set( - 1, 1.75, 1 );
     this.dirLight.position.multiplyScalar( 30 );
-    this.scene.add( this.dirLight );
+
 
     this.dirLight.castShadow = true;
-
     this.dirLight.shadow.mapSize.width = 2048;
     this.dirLight.shadow.mapSize.height = 2048;
 
-    let d = 50;
-
+    // TODO
+    const d = 50;
     this.dirLight.shadow.camera.left = - d;
     this.dirLight.shadow.camera.right = d;
     this.dirLight.shadow.camera.top = d;
     this.dirLight.shadow.camera.bottom = - d;
 
-    this.dirLight.shadow.camera.far = 3500;
+    this.dirLight.shadow.camera.far = 2500;
     this.dirLight.shadow.bias = - 0.0001;
 
+    this.scene.add( this.dirLight );
     this.scene.add( this.dirLightHeper );
 
     // GROUND
 
     this.groundMat.color.setHSL( 0.095, 1, 0.75 );
-
     this.ground.position.y = - 33;
     this.ground.rotation.x = - Math.PI / 2;
     this.ground.receiveShadow = true;
@@ -147,11 +158,8 @@ export class ViewportComponent implements OnInit {
     // SKYDOME
 
     this.uniforms[ 'topColor' ].value.copy( this.hemiLight.color );
-
     this.scene.fog.color.copy( this.uniforms[ 'bottomColor' ].value );
-
     this.scene.add( this.sky );
-
   }
 
   initAudio(): void {
