2bb3220d5f11732b4094e29747cc26cc9ec52cf6
patagona-trettig <t.rettig@patagona.de>
2020-04-09 21:27:32 +0200

    prepare routing and add multiple components

---
+++ src/app/profile-display/profile-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-display',
+  templateUrl: './profile-display.component.html',
+  styleUrls: ['./profile-display.component.css']
+})
+export class ProfileDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
