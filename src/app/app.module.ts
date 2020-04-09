f286b15f25a7f621d6276612e32aac5036099e97
DanielLiebler <DLiebler13@gmail.com>
2020-04-09 15:42:04 +0200

    clean Baby ass ಠ_ಠ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MessageComponent } from './message/message.component';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { MessageComponent } from './message/message.component';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    MatSelectModule,
     MatFormFieldModule
   ],
   providers: [],
