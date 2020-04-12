860654338cc06a6e6ee7ce2197b90c843c898023
patagona-trettig <t.rettig@patagona.de>
2020-04-13 01:09:23 +0200

    Clean up login component

--- src/app/services/maria.service.ts
+++ src/app/services/maria.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../model/User';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,19 +10,4 @@ export class MariaService {
 
   constructor(private httpClient: HttpClient) {}
 
-  addUsers(usr: User): Observable<any> {
-    return this.httpClient.put(this.userEndpoint, usr);
-  }
-
-  getUser(name: string): Observable<any> {
-    return this.httpClient.get(this.userEndpoint + '?login_name=' + name);
-  }
-
-  login(name: string, password: string): Observable<any> {
-    return this.httpClient.get(this.userEndpoint).pipe( map(users => {
-      // console.log(users, name, password_plain);
-      const authenticatedUser = (users as any[]).find( u => u['login_name'] === name && u['password_hash'] === password);
-      return !!authenticatedUser;
-    }));
-  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,8 @@ import {Observable, pipe} from 'rxjs';
 import {User} from '../model/User';
 import {query} from '@angular/animations';
 import {map} from 'rxjs/operators';
+import * as queryString from 'querystring';
+import {APIResponse} from '../model/APIResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +17,30 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   getUserById(user_id: number): Observable<User> {
-    return this.getUser(user_id).pipe(
-      map( users => users[0] )
-    );
+    const requestUrl = this.userEndpoint + '?user_id=' + user_id;
+    return this.httpClient
+      .get<User[]>(requestUrl)
+      .pipe(map(users => users[0]));
   }
 
-  getUser(user_id?: number, login_name?: string): Observable<User[]> {
-    const queryString: string = (user_id || '') + (login_name || '');
-    const requestUrl = this.userEndpoint + (queryString ? ('?' + queryString) : '');
-    return this.httpClient.get<User[]>(requestUrl);
+  getUserByLoginName(login_name: string): Observable<User> {
+    const requestUrl = this.userEndpoint + '?login_name=' + login_name;
+    return this.httpClient
+      .get<User[]>(requestUrl)
+      .pipe(map(users => users[0]));
+  }
+
+  removeUser(user_id: number): Observable<number> {
+    const requestUrl = this.userEndpoint + '?user_id=' + user_id;
+    return this.httpClient.delete<number>(requestUrl);
+  }
+
+  addUser(user: User): Observable<any> {
+    return this.httpClient.post(this.userEndpoint, user);
+  }
+
+  loginUser(login_name: string, password_hash: string): Observable<APIResponse<User[]>> {
+    return this.httpClient.post<APIResponse<User[]>>(this.userEndpoint + '/login', {login_name: login_name, password_hash: password_hash});
   }
 
 }
