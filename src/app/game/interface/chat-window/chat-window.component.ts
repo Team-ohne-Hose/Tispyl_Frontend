105ab6491bb5704e0b02c340c82b3056dbd9c11b
Manuel Stolze <manu.stolze93@yahoo.de>
2021-02-28 00:35:20 +0100

    remove console log

--- src/app/game/interface/chat-window/chat-window.component.ts
+++ src/app/game/interface/chat-window/chat-window.component.ts
@@ -71,10 +71,7 @@ export class ChatWindowComponent implements ColyseusNotifyable {
 
     } else if (keyEvent.key === 'ArrowUp') {
 
-      // console.log(this.messageHistory.length - 1, this.historyIndex);
-
       if (this.historyIndex <= this.messageHistory.length - 1) {
-        // console.log(this.currentMessage[this.historyIndex]);
         this.currentMessage = this.messageHistory[this.historyIndex] || '';
         this.historyIndex = this.historyIndex + 1;
       } else {
