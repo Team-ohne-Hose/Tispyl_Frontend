51a80143cb6df0706bc6535b114072c455e5a1ea
Tizian Rettig <tizian.rettig@stud.h-da.de>
2021-06-03 16:41:24 +0200

    Add news page features

---
+++ src/app/components/lobby/md-content-list/md-content.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appMdContent]'
+})
+export class MdContentDirective {
+  constructor(public viewContainerRef: ViewContainerRef) { }
+}
---
+++ src/app/components/lobby/news/resolve-to-headline.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { MarkdownContentService } from '../../../services/markdown-content.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Pipe({
+  name: 'resolveToHeadline'
+})
+export class ResolveToHeadlinePipe implements PipeTransform {
+
+  constructor(private mdc: MarkdownContentService) {}
+
+  /**
+   * Translates markdown file names to their headline
+   * @param markdownName name of the file that will be translated through the cache
+   */
+  transform(markdownName: string): Observable<string> {
+    return this.mdc.headlineCache.pipe(
+        map( cache => cache !== undefined ? cache[markdownName] : 'undefined' )
+    );
+  }
+
+}
