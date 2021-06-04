29d1cbae284bd20a1b8e254beeb0c795280e9124
Manuel Stolze <manu.stolze93@yahoo.de>
2021-06-05 00:49:09 +0200

    fix dummy-error

--- src/app/components/debugdummy/debugdummy.component.ts
+++ src/app/components/debugdummy/debugdummy.component.ts
@@ -1,7 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable no-empty-function */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-debugdummy',
   templateUrl: './debugdummy.component.html',
   styleUrls: ['./debugdummy.component.css'],
 })
-export class DebugdummyComponent {}
+export class DebugdummyComponent {
+  constructor() {}
+
+  defaultFunc() {}
+}
