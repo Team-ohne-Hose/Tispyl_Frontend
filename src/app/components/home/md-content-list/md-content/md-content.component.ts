4e88376400c98189a1eb3df7d232c89f25f76a95
Tizian Rettig <tizian.rettig@stud.h-da.de>
2021-08-04 19:01:50 +0200

    Add first actual news

--- src/app/components/home/md-content-list/md-content/md-content.component.ts
+++ src/app/components/home/md-content-list/md-content/md-content.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MarkdownContentService, SourceDirectory } from '../../../../services/markdown-content.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MarkdownToHtmlPipe } from 'markdown-to-html-pipe/src/markdown-to-html.pipe';
 
 @Component({
   selector: 'app-md-content',
@@ -9,21 +11,18 @@ import { MarkdownContentService, SourceDirectory } from '../../../../services/ma
 export class MdContentComponent {
   mdText = undefined;
   anchorTag = undefined;
-  constructor(private mcs: MarkdownContentService) {}
+  constructor(private mcs: MarkdownContentService, private sanitizer: DomSanitizer) {}
 
   /**
    * Loads a markdown file based on its name and inlines a parsed version as HTML
-   * This inlines arbitrary html and thus is not sanitized or necessarily clean. This should be corrected.
-   * TODO: Fix this by adding the additional pipe described in the blog below
-   *
-   * @see https://medium.com/angular-in-depth/warning-sanitizing-html-stripped-some-content-and-how-to-deal-with-it-properly-10ff77012d5a
    * @param dir source directory on the server
    * @param fileName file name of the file in the source directory
    */
   load(dir: SourceDirectory, fileName: string): void {
     this.mcs.getMarkdownFor(dir, fileName).subscribe(
       (md: string) => {
-        this.mdText = md;
+        const p = new MarkdownToHtmlPipe();
+        this.mdText = this.sanitizer.bypassSecurityTrustHtml(p.transform(md));
         this.anchorTag = fileName;
       },
       (error) => {
