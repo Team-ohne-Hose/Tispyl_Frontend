cc41a779bb02ea51cceb744c3c603d48af38c5d7
Manuel Stolze <manu.stolze93@yahoo.de>
2021-08-29 17:48:39 +0200

    edit profile with angular validation

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable no-empty-function */
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component } from '@angular/core';
 import { MailerService } from 'src/app/services/mailer.service';
 import { AppToastService } from 'src/app/services/toast.service';
@@ -18,8 +15,8 @@ export class ContactMessage {
 export class ContactComponent {
   constructor(private mailerService: MailerService, private toastService: AppToastService) {}
 
-  onClickSubmit(contactMessage: ContactMessage) {
-    this.mailerService.submitMail(contactMessage.email, contactMessage.msg).subscribe((re) => {
+  onClickSubmit(contactMessage: ContactMessage): void {
+    const res = this.mailerService.submitMail(contactMessage.email, contactMessage.msg).subscribe((re) => {
       if (re.success) {
         this.toastService.show(
           'Init',
