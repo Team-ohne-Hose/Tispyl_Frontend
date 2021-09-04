6b500077d7e8217dc46549870440cec6f5b645e3
Manuel Stolze <63359026+manuelstolze@users.noreply.github.com>
2021-09-05 01:18:13 +0200

    Revert "progress"
    This reverts commit 04eca2bee1e3fa3e3b7c23959016b09a88474fe7.

--- src/app/components/home/profile/edit-profile/edit-profile.component.ts
+++ src/app/components/home/profile/edit-profile/edit-profile.component.ts
@@ -1,11 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { BasicUser } from 'src/app/services/user.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['../../../../../../src/assets/css/styles.css', './edit-profile.component.css'],
+  styleUrls: ['./edit-profile.component.css'],
 })
-export class EditProfileComponent {
-  @Input() currentUser: BasicUser;
-}
+export class EditProfileComponent {}
