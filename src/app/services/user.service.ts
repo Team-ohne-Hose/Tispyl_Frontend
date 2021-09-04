bccef7be0cf6675f72c2ba05237f4794042ec36e
Manuel Stolze <63359026+manuelstolze@users.noreply.github.com>
2021-09-05 01:18:13 +0200

    Revert "change profile"
    This reverts commit 00dd6861da4758c21b0d90d3ff246fad8304783c.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,9 +4,6 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { APIResponse } from '../model/APIResponse';
 import { environment } from '../../environments/environment';
 import * as hash from 'object-hash';
-import { EditUserData } from '../components/home/profile/edit-profile/edit-profile.component';
-import { map } from 'rxjs/operators';
-import { AppToastService } from './toast.service';
 
 export class BasicUser {
   id: number;
@@ -24,6 +21,38 @@ export class LoginUser extends BasicUser {
   password_hash;
 }
 
+// export class OldUser {
+//   id: number;
+//   login_name: string;
+//   display_name: string;
+//   password_hash: string;
+//   user_creation: string;
+//   time_played: number;
+//   profile_picture: string;
+//   last_figure: string;
+//   is_connected: boolean;
+//   is_dev: boolean;
+
+//   constructor(login: string, display: string, password: string) {
+//     this.login_name = login;
+//     this.display_name = display;
+//     this.password_hash = hash.MD5(password);
+//   }
+// }
+
+// export class OldLoginUser {
+//   id: number;
+//   login_name: string;
+//   display_name: string;
+//   user_creation: string;
+//   time_played: number;
+//   profile_picture: string;
+//   last_figure: string;
+//   is_connected: boolean;
+//   is_dev: boolean;
+// }
+// export class OldForeignUser extends OldLoginUser { }
+
 @Injectable({
   providedIn: 'root',
 })
@@ -31,9 +60,9 @@ export class UserService {
   activeUser: BehaviorSubject<LoginUser>;
   profileUser: BasicUser;
 
-  private endpoint = environment.endpoint + 'user';
+  private userEndpoint = environment.endpoint + 'user';
 
-  constructor(private httpClient: HttpClient, private toastService: AppToastService) {
+  constructor(private httpClient: HttpClient) {
     this.activeUser = new BehaviorSubject<LoginUser>(undefined);
   }
 
@@ -52,17 +81,17 @@ export class UserService {
   // REQUESTS
 
   getUserByLoginName(login_name: string): Observable<APIResponse<BasicUser>> {
-    return this.httpClient.get<APIResponse<BasicUser>>(this.endpoint + '?login_name=' + login_name);
+    return this.httpClient.get<APIResponse<BasicUser>>(this.userEndpoint + '?login_name=' + login_name);
   }
 
   removeUser(user_id: number): Observable<number> {
-    const requestUrl = this.endpoint + '?user_id=' + user_id;
+    const requestUrl = this.userEndpoint + '?user_id=' + user_id;
     return this.httpClient.delete<number>(requestUrl);
   }
 
   syncUserData(user: LoginUser): void {
     this.httpClient
-      .get<APIResponse<LoginUser>>(this.endpoint + '?login_name=' + user.login_name)
+      .get<APIResponse<LoginUser>>(this.userEndpoint + '?login_name=' + user.login_name)
       .subscribe((response) => {
         if (response.payload !== undefined) {
           this.setActiveUser(response.payload as LoginUser);
@@ -73,26 +102,7 @@ export class UserService {
   }
 
   requestUserDatabyId(userId: number): Observable<APIResponse<BasicUser>> {
-    const requestUrl = this.endpoint + '/byId?userId=' + userId;
+    const requestUrl = this.userEndpoint + '/byId?userId=' + userId;
     return this.httpClient.get<APIResponse<BasicUser>>(requestUrl);
-  }
-
-  updateUser(userData: EditUserData) {
-    const requestUrl = this.endpoint;
-
-    this.httpClient.patch<APIResponse<LoginUser>>(requestUrl, userData).subscribe((response) => {
-      if (!response.success) {
-        this.toastService.show(
-          'Error',
-          'Beim aktualisieren deines Profils ist etwas schief gelaufen.',
-          'bg-danger text-light',
-          3000
-        );
-        return;
-      }
-
-      this.activeUser.next(response.payload);
-      this.toastService.show('Success', 'Dein Profil wurde erfolgreich aktualisiert.', 'bg-success text-light', 3000);
-    });
   }
 }
