6a2f6e7dc396d9a098f33f34f3c668fd6e44611e
Manuel Stolze <63359026+manuelstolze@users.noreply.github.com>
2021-09-05 01:18:13 +0200

    Revert "progress"
    This reverts commit 04eca2bee1e3fa3e3b7c23959016b09a88474fe7.

--- src/app/components/home/profile/profile.component.ts
+++ src/app/components/home/profile/profile.component.ts
@@ -24,18 +24,12 @@ export class ProfileComponent implements OnInit {
   isCurrentUser: boolean;
   foreignUser: BasicUser;
 
-  isShownLastGames = true;
-
   constructor(
     private route: ActivatedRoute,
     private changeDetector: ChangeDetectorRef,
     private userService: UserService,
     private fileService: FileService
   ) {}
-
-  toggleShow() {
-    this.isShownLastGames = !this.isShownLastGames;
-  }
 
   private onSuccess() {
     this.selectedFile.pending = false;
