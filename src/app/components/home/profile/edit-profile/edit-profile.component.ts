8a2dcea99d27863a8da09b5bedaf626435d89cb4
Manuel Stolze <63359026+manuelstolze@users.noreply.github.com>
2021-09-05 01:18:13 +0200

    Revert "edit profile with angular validation"
    This reverts commit cc41a779bb02ea51cceb744c3c603d48af38c5d7.

--- src/app/components/home/profile/edit-profile/edit-profile.component.ts
+++ src/app/components/home/profile/edit-profile/edit-profile.component.ts
@@ -2,14 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import * as hash from 'object-hash';
 import { BasicUser, UserService } from 'src/app/services/user.service';
 import { AppToastService } from 'src/app/services/toast.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { EqualityValidator } from '../../login/EqualityValidator';
-import { CustomValidators } from '../../../../validators/custom-validators';
 
 export class EditUserData {
-  id: any;
+  id: number;
   login_name?: string;
-  display_name: string;
+  display_name?: string;
   currentPassword: string;
   newPassword?: string;
   newPasswordConfirmed?: string;
@@ -20,72 +17,29 @@ export class EditUserData {
   templateUrl: './edit-profile.component.html',
   styleUrls: ['../../../../../../src/assets/css/styles.css', './edit-profile.component.css'],
 })
-export class EditProfileComponent implements OnInit {
-  constructor(private userService: UserService, private toastService: AppToastService) {}
-
+export class EditProfileComponent {
   @Input() currentUser: BasicUser;
 
-  /** input values */
-  username = '';
-  currentPassword = '';
-  newPassword = '';
-  newPasswordConfirmed = '';
+  constructor(private userService: UserService, private toastService: AppToastService) {}
 
-  submitted = false;
-
-  /** Registration auxiliaries */
-  isSuccessText = false;
-
-  // edit form
-  edit = null;
-
-  onSubmit() {
-    const editUser: EditUserData = new EditUserData();
-    editUser.id = this.currentUser.id;
-    editUser.currentPassword = hash.MD5(this.edit.value.currentPassword);
-    editUser.display_name = this.edit.value.newUsername;
-    this.userService.updateUser(editUser);
-  }
-
-  ngOnInit(): void {
-    this.edit = new FormGroup(
-      {
-        newUsername: new FormControl(this.currentUser.display_name, [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(48),
-        ]),
-        currentPassword: new FormControl('', [Validators.minLength(8), Validators.required]),
-        newPassword: new FormControl('', [Validators.minLength(8), Validators.maxLength(64)]),
-        newPasswordConfirmed: new FormControl('', [Validators.minLength(8), Validators.maxLength(64)]),
-      },
-      [EqualityValidator('newPassword', 'newPasswordConfirmed')]
-    );
-  }
-
-  keyPressed(keyEvent: KeyboardEvent): void {
-    console.log(this.edit);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   submitUserChanges(changedUser: EditUserData) {
     changedUser.id = this.currentUser.id;
 
-    if (changedUser.currentPassword.length === 0) {
+    if (changedUser.currentPassword.length === 0)
       this.toastService.show('Error', 'Du hast kein Passwort angegeben.', 'bg-danger text-light', 3000);
-    } else if (
+    else if (
       (changedUser.newPassword.length !== 0 && changedUser.newPasswordConfirmed.length === 0) ||
       (changedUser.newPassword.length === 0 && changedUser.newPasswordConfirmed.length !== 0)
-    ) {
+    )
       this.toastService.show('Error', 'Bitte wiederhole dein neues Passwort.', 'bg-danger text-light', 3000);
-    } else if (changedUser.newPassword !== changedUser.newPasswordConfirmed) {
+    else if (changedUser.newPassword !== changedUser.newPasswordConfirmed)
       this.toastService.show(
         'Error',
         'Die eingegebenen Passwörter stimmen nicht überein.',
         'bg-danger text-light',
         3000
       );
-    } else {
+    else {
       // hash passwords
       changedUser.newPassword = hash.MD5(changedUser.newPassword);
       changedUser.currentPassword = hash.MD5(changedUser.currentPassword);
