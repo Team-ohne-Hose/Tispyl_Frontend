2961a2291250f873b9dacb693e2416caa020484c
Manuel Stolze <63359026+manuelstolze@users.noreply.github.com>
2021-09-05 01:18:13 +0200

    Revert "imports and returns"
    This reverts commit df095922689dd70372ae331e86e908d13f828c45.

--- src/app/components/home/profile/edit-profile/edit-profile.component.ts
+++ src/app/components/home/profile/edit-profile/edit-profile.component.ts
@@ -4,6 +4,7 @@ import { BasicUser, UserService } from 'src/app/services/user.service';
 import { AppToastService } from 'src/app/services/toast.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { EqualityValidator } from '../../login/EqualityValidator';
+import { CustomValidators } from '../../../../validators/custom-validators';
 
 export class EditUserData {
   id: any;
