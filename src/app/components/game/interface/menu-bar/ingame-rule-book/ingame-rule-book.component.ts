9e5421337a26c7606daac143c36c97f9cc830607
Tizian Rettig <tizian.rettig@stud.h-da.de>
2021-09-13 15:03:55 +0200

    Add static rules to the ingame rulebook

--- src/app/components/game/interface/menu-bar/ingame-rule-book/ingame-rule-book.component.ts
+++ src/app/components/game/interface/menu-bar/ingame-rule-book/ingame-rule-book.component.ts
@@ -3,6 +3,11 @@ import { GameActionType, MessageType } from '../../../../../model/WsData';
 import { GameStateService } from '../../../../../services/game-state.service';
 import { Rule } from '../../../../../model/state/Rule';
 
+interface StaticRule {
+  text: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-ingame-rule-book',
   templateUrl: './ingame-rule-book.component.html',
@@ -10,6 +15,21 @@ import { Rule } from '../../../../../model/state/Rule';
 })
 export class IngameRuleBookComponent {
   @Input() rules: Rule[] = [];
+
+  staticRules: StaticRule[] = [
+    {
+      text: 'Kommst du auf ein Feld musst du die Aufgabe auf diesem ausführen.',
+      icon: 'fas fa-running',
+    },
+    { text: 'Ein 0,33L sollte in ca. acht Schlücken leer sein.', icon: 'fas fa-ruler-combined' },
+    {
+      text: 'Kritik am Spiel wird mit 2 Strafschlücken geahndet.',
+      icon: 'fas fa-microphone-alt-slash',
+    },
+    { text: 'Jede Aktion muss versucht werden.', icon: 'far fa-dizzy' },
+    { text: 'Fehlgeschlagene Aufgaben werden mit 2 Strafschlücken bestraft.', icon: 'fas fa-beer' },
+    { text: 'Wer mit spielt akzeptiert automatisch alle Regeln.', icon: 'fas fa-balance-scale' },
+  ];
 
   constructor(private gameState: GameStateService) {}
 
